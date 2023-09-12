ada47a02a41ee185fe6a2660d3cfc0e6d650601b
NaufanZaki <nzluqmanulhakim@gmail.com>
2023-09-12 21:12:38 +0700

    Initial Commit

---
+++ tugas1Strukdat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tugas1Strukdat
+//
+//  Created by zaki on 12/09/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
